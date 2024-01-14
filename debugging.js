3c4ff64c7869d1a0196aa76f08c7577425d6e08f
gospel <gospel4levi@gmail.com>
2024-01-14 03:36:55 +0100

    saving w1 to repository

---
+++ debugging.js
@@ -0,0 +1,13 @@
+const PI = 3.14;
+const radius = 3;
+let area = 0;
+area = radius * radius * pi;
+radius = 4;
+area = radius * radius * pi;
+
+let firstName = "Anthonia Francesca";
+
+let num1 = 100;
+let num2 = '10';
+
+const sum = num2 + num1;
