ad8272fe26d04bc366d1bc803d1ca09ff53d3186
gospel <gospel4levi@gmail.com>
2024-02-05 00:54:48 +0100

    committing changes to w04

--- scripts/w04-task.js
+++ scripts/w04-task.js
@@ -1,28 +1,78 @@
 /* LESSON 3 - Programming Tasks */
 
-/* Profile Object  */
+/* Profile Object */
+let myProfile = {
+    name: 'Gospel Levi',
+    photo: {
+      src: 'images/mypic.jpg',
+      alt: 'My Profile Picture'
+    },
+    favoriteFoods: [
+      'Garri',
+      'Beans',
+      'Moi-Moi',
+      'Rice',
+      'Oat Meal',
+      'Peppersoup',
+      'Noodles'
+    ],
+    hobbies: [
+      'Soccer video games',
+      'Swimming',
+      'Surfing the net'
+    ],
+    placesLived: [] // Initialize the placesLived array
+  };
+  
+  /* Populate Profile Object with placesLive objects */
+  myProfile.placesLived.push(
+    {
+      place: 'Ikeja, Lagos',
+      length: '5 years'
+    },
+    {
+      place: 'Enugu, Enugu',
+      length: '2 years'
+    },
+    {
+      place: 'Port Harcourt, Rivers',
+      length: '2 years'
+    }, 
+    {
+      place: 'Ogwashiuku, Delta',
+      length: '5 months'
+    }
+  );
+  
+  /* DOM Manipulation - Output */
+  /* Name */
+  document.querySelector("#name").textContent = myProfile.name;
+  
+  /* Photo with attributes */
+  document.querySelector('#photo').src = myProfile.photo.src;
+  
+  /* Favorite Foods */
+  myProfile.favoriteFoods.forEach(food => {
+    let listItem = document.createElement("li");
+    listItem.textContent = food;
+    document.querySelector('#favorite-foods').appendChild(listItem);
+  });
 
+  /* Hobbies */
+  let hobbiesList = document.querySelector("#hobbies");
+  myProfile.hobbies.forEach(function(hobby) { //Using function expression
+    let listItem = document.createElement("li");
+    listItem.textContent = hobby;
+    hobbiesList.appendChild(listItem);
+  });
 
-
-
-/* Populate Profile Object with placesLive objects */
-
-
-
-
-/* DOM Manipulation - Output */
-
-/* Name */
-
-/* Photo with attributes */
-
-
-/* Favorite Foods List*/
-
-
-/* Hobbies List */
-
-
-/* Places Lived DataList */
-
-
+  /* Places Lived */
+  let placesLivedList = document.querySelector("#places-lived");
+  myProfile.placesLived.forEach(place => {
+    let dt = document.createElement("dt");
+    dt.textContent = place.place;
+    let dd = document.createElement("dd");
+    dd.textContent = `for ${place.length}`;
+    placesLivedList.appendChild(dt);
+    placesLivedList.appendChild(dd);
+  });
